1fa9f10baab7b6482f6f0f64a0babefa3fc7492d
nareshkannasln <nareshkannashanmugam@gmail.com>
2026-01-27 13:07:48 +0530

    fix(department): allow company wise tree view

--- erpnext/setup/doctype/department/department_tree.js
+++ erpnext/setup/doctype/department/department_tree.js
@@ -2,24 +2,31 @@ frappe.treeview_settings["Department"] = {
 	ignore_fields: ["parent_department"],
 	get_tree_nodes: "erpnext.setup.doctype.department.department.get_children",
 	add_tree_node: "erpnext.setup.doctype.department.department.add_node",
+	get_tree_root: false,
+	root_label: __("Department"),
 	filters: [
 		{
 			fieldname: "company",
-			fieldtype: "Link",
-			options: "Company",
+			fieldtype: "Select",
 			label: __("Company"),
+			options: erpnext.utils.get_tree_options("company"),
+			default: erpnext.utils.get_tree_default("company"),
+			on_change: function () {
+				cur_tree && cur_tree.make_tree();
+			},
 		},
 	],
-	breadcrumb: "HR",
-	root_label: "All Departments",
-	get_tree_root: true,
-	menu_items: [
+	fields: [
 		{
-			label: __("New Department"),
-			action: function () {
-				frappe.new_doc("Department", true);
-			},
-			condition: 'frappe.boot.user.can_create.indexOf("Department") !== -1',
+			fieldtype: "Data",
+			fieldname: "department_name",
+			label: __("New Department Name"),
+			reqd: true,
+		},
+		{
+			fieldtype: "Check",
+			fieldname: "is_group",
+			label: __("Is Group"),
 		},
 	],
 	onload: function (treeview) {
